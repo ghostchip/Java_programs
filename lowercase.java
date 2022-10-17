dcfa05bf352928c20f76d0a9d8af9fd506856ac2
ghostchip <erickariuki476@gmail.com>
2022-10-17 15:59:10 +0300

    new files

---
+++ lowercase.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class lowercase {
+    public static void main(String[] args) {
+
+    Scanner input = new Scanner(System.in);
+
+    String word = input.nextLine();
+
+    System.out.print("Enter name ");
+    
+    System.out.println(word.toLowerCase());
+    
+}
+}
+
