f71a22cd101cca7398454bcdf77861776d3687a5
ghostchip <erickariuki476@gmail.com>
2022-10-27 10:01:51 +0300

    added two files

---
+++ odd.java
@@ -0,0 +1,10 @@
+public class odd {
+   public static void main(String args[]) {
+		System.out.println("The Odd Numbers are:");
+		for (int i = 30; i <= 50; i++) {
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+	} 
+}
